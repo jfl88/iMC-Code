d13826d43824efbc58cf8e3f837fa71f69faa42f
Jason Lyons <butters1@gmail.com>
2015-11-13 14:21:15 +1000

    update with SNMP

--- Hirschmann/HirschmannGeneric/reboot.tcl
+++ Hirschmann/HirschmannGeneric/reboot.tcl
@@ -4,16 +4,6 @@
 # Purpose:       reload switch (cold start)
 #**************************************************************************
 
-global $enforce_save
-
-if {$enforce_save == "true"} {
-    send "copy system:running-config nvram:startup-config\r"
-    expect "Are you sure you want to save? (y/n)"
-    send "y\r""
-    sleep 1
-    expect "Configuration Saved!"
-}
-
 send "reboot\r"
 # expect save dialog?
 # confirm reboot?
